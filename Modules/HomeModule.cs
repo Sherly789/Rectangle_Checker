d77c53ca8d26806627f174ef4dd73fbb29be8651
Shokouh Farvid <Shokouhu@example.com>
2016-07-13 13:01:59 -0700

    calculate surface area of cube

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,9 +11,18 @@ namespace RectangleChecker
         return View["rectangle_form.cshtml"];
       };
       Get["/rectangle_result"] = _ => {
-        Rectangle myRectangle = new Rectangle(Request.Query["side-length"], Request.Query["side-width"]);
-        return View["rectangle_result.cshtml", myRectangle];
-      };
+      Dictionary<string, object> Shapes = new Dictionary<string, object>();
+      Rectangle myRectangle = new Rectangle(Request.Query["side-length"], Request.Query["side-width"]);
+      Shapes.Add("ResultingRectangle", myRectangle);
+
+      if (myRectangle.IsSquare())
+      {
+        Cube myCube = new Cube(myRectangle);
+        Shapes.Add("ResultingCube", myCube);
+      }
+
+      return View["rectangle_result.cshtml", Shapes];
+    };
     }
   }
 }
